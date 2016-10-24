1b1463aceac095b36aaab6bc0135e48b0baf9da8
Min Park <jpark8@cca.edu>
2016-10-24 11:21:26 -0700

    edit

---
+++ projects/p2/assets/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+	
+	$('.an').click(function () {
+            $('.a').css({"color":"#feac54"});
+        }).mouseout(function() {
+        	$('.a').css({"color":"#252527"})
+        }
+    	
+    
+    );	
+   
+
+    $('.ae').mouseover(function () {
+            $('.aaa').css({"color":"#feac54"});
+        }).mouseout(function() {
+        	$('.aaa').css({"color":"#252527"})
+        }
+    	
+    
+    );	
+    $('.be').mouseover(function () {
+            $('.bbb').css({"color":"#feac54"});
+        }).mouseout(function() {
+        	$('.bbb').css({"color":"#252527"})
+        }
+    	
+    
+    );	
+    $('.c').mouseover(function () {
+            $('.ccc').css({"color":"#feac54"});
+        }).mouseout(function() {
+        	$('.ccc').css({"color":"#252527"})
+        }
+    	
+    
+    );	   
+    $('.d').mouseover(function () {
+            $('.ddd').css({"color":"#feac54"});
+        }).mouseout(function() {
+        	$('.ddd').css({"color":"#252527"})
+        }
+    	
+    
+    );	   
+    $('.bn').click(function () {
+            $('.num').css({"color":"#feac54"});
+        }).mouseout(function() {
+            $('.num').css({"color":"#252527"})
+        }
+        
+    
+    );      
+   
+
+	
+
+});
